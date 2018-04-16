a5e54d02e1bde3058cdb92c1b21bb7226429ea3c
AlaaAttya <alaa.attya91@gamil.com>
2018-04-16 21:08:04 +0200

    update contribution

---
+++ files/map.mm
@@ -0,0 +1,191 @@
+
+<map version="0.9.0">
+    <node TEXT="architect" FOLDED="false" POSITION="right" ID="5ad4f218098a8ad0b89d7d95" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#666666" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="visual design :" FOLDED="false" POSITION="right" ID="6ce38d33089dee106705eca0e3f69b03">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="sequence diagram" FOLDED="false" POSITION="right" ID="953b3742b461ce52f733d4edfc0c2231">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="interaction diagram" FOLDED="false" POSITION="right" ID="9ff9d3cdf5ba2419fe3e89efaa0196c1">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="ERD" FOLDED="false" POSITION="right" ID="ca970a4553b146d142a3eb92dd07c9c6">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="UML" FOLDED="false" POSITION="right" ID="ab45517c5b6b8e030ea843f2298da750">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="class diagram" FOLDED="false" POSITION="right" ID="e6b7f196a2b9f27929ee026f1674e805">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="architectural patterns " FOLDED="false" POSITION="left" ID="4f383d3d2d2691b5d81f7265c5158dea">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="domain driven design" FOLDED="false" POSITION="left" ID="1962f8546b57ca875b537a903440e6ea">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="microservices" FOLDED="false" POSITION="left" ID="def56859033890838ed2121e3dce691d">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="service oriented" FOLDED="false" POSITION="left" ID="34babe5f9f450f04b6cb356e0616bf30">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="MVC" FOLDED="false" POSITION="left" ID="f5fcfe1c52bb88962972fc85165c3faa">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="client/server" FOLDED="false" POSITION="left" ID="0c80e2c94cef927202478b3d018df13f">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="layered architecture" FOLDED="false" POSITION="left" ID="e3df0eddbb6b748d7d7feb96f6b41755">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="design patterns" FOLDED="false" POSITION="right" ID="289a7b54fda179ec1998d9c9a8109443">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="creational" FOLDED="false" POSITION="right" ID="8fde2961aa2902e6515d026abe3fd063">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="builder" FOLDED="false" POSITION="right" ID="6ceba318e3b3acdac8a987e7a052afe8">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="prototype" FOLDED="false" POSITION="right" ID="171341e89e2bc5c239897c4e90d1d19d">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="factory" FOLDED="false" POSITION="right" ID="ccc3d5637e8ebae035ce9aa68d011bb2">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="singletone" FOLDED="false" POSITION="right" ID="d6dfcf2eb57e193f485351c53cd85e50">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="service locator" FOLDED="false" POSITION="right" ID="4a02312da1741a91772c8a41a5cfe991">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="structural" FOLDED="false" POSITION="right" ID="ed283f395067fffb1e26bda487c7c05d">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Facade" FOLDED="false" POSITION="right" ID="f44094717813adcfac5c986d6a6be87c">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Decorator" FOLDED="false" POSITION="right" ID="9422568aa0b1b4188dee38858eb2208b">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Adapter" FOLDED="false" POSITION="right" ID="1835d90f4d71a5a17b6ef140c7eafd38">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Proxy" FOLDED="false" POSITION="right" ID="927c3c729fcda12d2e7cc184d3f019ef">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="behavioral" FOLDED="false" POSITION="right" ID="1be104d78739683893853d24b6453f86">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="iterator" FOLDED="false" POSITION="right" ID="59b5b07f0feb1a35e9363d4b7b5990ca">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="strategy" FOLDED="false" POSITION="right" ID="f6a142135d50d6b045ea05ebb1d4d597">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="observer" FOLDED="false" POSITION="right" ID="8477dd74fc03428e162834b9abc01328">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="command" FOLDED="false" POSITION="right" ID="0ee968255bccdfc6e9cc6e247bbb333c">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="reading and thought leaders" FOLDED="false" POSITION="left" ID="2eaa2f3eb211115938f3e31048d18692">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="gang of four (GoF)" FOLDED="false" POSITION="left" ID="f3e7de40ac1b773cc72d5c74f75bda80">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Eric gamma" FOLDED="false" POSITION="left" ID="22d6acd4a7984646e3c1999aef16b3a7">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Richard Helm" FOLDED="false" POSITION="left" ID="d2ee58514c01ff365e8c4c9eac74d1fb">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Ralph Johnson" FOLDED="false" POSITION="left" ID="fd8d4d5cce1f370784667fc329fce46d">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="John Vlissides" FOLDED="false" POSITION="left" ID="fb2d3d137bf85d52346c44a81df73c4d">
+                    <edge COLOR="#666666" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Martin fowler" FOLDED="false" POSITION="left" ID="897c31d2271dc90a323bacda7272de3a">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Eric evans" FOLDED="false" POSITION="left" ID="1ea1d011f6b2618c84f87d3374666d3d">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="communication patterns" FOLDED="false" POSITION="left" ID="986091605e024217e0e29a1016154e99">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="event sourcing" FOLDED="false" POSITION="left" ID="c7783a3dcd6774f7cd13e30049887e4f">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="enterprise service bus" FOLDED="false" POSITION="left" ID="54a4a1985d66ea7c96e234fc668a7772">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="remote procedure call (RPC)" FOLDED="false" POSITION="left" ID="c556859ec18abfcc5af68eaeb96360b4">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="message streaming" FOLDED="false" POSITION="left" ID="1c992c81788e5aa6f9141c02054b7535">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="software engineering" FOLDED="false" POSITION="right" ID="37d1cbca6b8406a247252626fb768818">
+            <edge COLOR="#666666" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="clean architecture" FOLDED="false" POSITION="right" ID="536f98b4e12ce6746a72e7a766f91eec">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="SOLID principles" FOLDED="false" POSITION="right" ID="44d19ba04ad5b153cecc108de82c03e3">
+                <edge COLOR="#666666" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+    </node>
+</map>
